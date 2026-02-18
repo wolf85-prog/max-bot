5beb75caa30c250d2e26b73533ed763eb8dfe717
Sergey <seregaland@yandex.ru>
2026-02-18 11:25:34 +0300

    fix send bot

--- botmax.js
+++ botmax.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 
 //MAX api
-const { MaxApi } = require('@maxhub/max-bot-api');
-//import { MaxApi } from '@maxhub/max-bot-api';
+const { Bot } = require('@maxhub/max-bot-api');
 
 const token = process.env.MAX_API_TOKEN
 
-const bot = new MaxApi(token);
+const bot = new Bot(token);
 
 // Обработчик для команды '/start'
 bot.command('start', (ctx) => ctx.reply('Добро пожаловать!'));
@@ -14,21 +13,42 @@ bot.command('start', (ctx) => ctx.reply('Добро пожаловать!'));
 // Установка подсказок с доступными командами
 bot.api.setMyCommands([
   { 
-    name: 'ping',
-    description: 'Сыграть в пинг-понг'
+    name: 'menu',
+    description: 'Меню'
+  },
+  { 
+    name: 'info',
+    description: 'Информация'
   },
 ]);
 
 // Обработчик события запуска бота
-bot.on('bot_started', (ctx) => ctx.reply('Привет! Отправь мне команду /ping, чтобы сыграть в пинг-понг'));
+//bot.on('bot_started', (ctx) => ctx.reply('Привет! Отправь мне команду /ping, чтобы сыграть в пинг-понг'));
 
-// Обработчик команды '/ping'
-bot.command('ping', (ctx) => ctx.reply('pong'));
+// Обработчик команды '/info'
+bot.command('info', (ctx) => 
+  ctx.reply('Информация')    
+);
+
+// Обработчик команды '/menu'
+bot.command('menu', (ctx) => 
+  ctx.reply('Меню')
+);
 
 // Обработчик для сообщения с текстом 'hello'
-bot.hears('hello', (ctx) => ctx.reply('world'));
+bot.hears('hello', (ctx) =>
+   ctx.reply('world')
+);
 
 // Обработчик для всех остальных входящих сообщений
-bot.on('message_created', (ctx) => ctx.reply(ctx.message.body.text));
+//bot.on('message_created', (ctx) => ctx.reply(ctx.message.body.text));
+
+// Обработчик для любого другого сообщения
+bot.on('message_created', (ctx) => {
+    ctx.reply(ctx.message.body.text)
+    const message = ctx.message; // Полученное сообщение
+    console.log(message)
+});
+
 
 bot.start();
